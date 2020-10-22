770f8adc213f21dff8099760497206657cffa9e8
HuyHQ <huyha14082012@gmail.com>
2020-10-22 16:49:46 +0700

    add react scroll

--- src/components/Navbar/navbar.component.jsx
+++ src/components/Navbar/navbar.component.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaBars } from 'react-icons/fa';
+import { IconContext } from 'react-icons/lib';
+import { animateScroll as scroll } from 'react-scroll';
 import {
     Nav,
     NavbarContainer,
@@ -13,26 +15,69 @@ import {
 } from './navbar.styles';
 
 const Navbar = ({ toggle }) => {
+    const [ scrollNav, setScrollNav ] = useState(false);
+
+    const changeNav = () => {
+        if (window.scrollY > 80) {
+            setScrollNav(true);
+        } else {
+            setScrollNav(false);
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', changeNav);
+    }, []);
+
+    const toggleHome = () => {
+        scroll.scrollToTop();
+    }
+
     return (
         <>
-            <Nav>
+            <IconContext.Provider value={{ color: '#fff'}}>
+                <Nav scrollNav={scrollNav}>
                 <NavbarContainer>
-                    <NavLogo to='/'>kiguri</NavLogo>
+                    <NavLogo to='/' onClick={toggleHome}>kiguri</NavLogo>
                     <MobileIcon onClick={toggle}>
                         <FaBars />
                     </MobileIcon>
                     <NavMenu>
                         <NavItem>
-                            <NavLinks to='about'>About</NavLinks>
+                            <NavLinks to='about'
+                                      smooth={true}
+                                      duration={500}
+                                      spy={true}
+                                      exact='true'
+                                      offet={-80}
+                            >About</NavLinks>
                         </NavItem>
                         <NavItem>
-                            <NavLinks to='discover'>Discover</NavLinks>
+                            <NavLinks to='discover'
+                                      smooth={true}
+                                      duration={500}
+                                      spy={true}
+                                      exact='true'
+                                      offet={-80}
+                            >Discover</NavLinks>
                         </NavItem>
                         <NavItem>
-                            <NavLinks to='services'>Services</NavLinks>
+                            <NavLinks to='services'
+                                      smooth={true}
+                                      duration={500}
+                                      spy={true}
+                                      exact='true'
+                                      offet={-80}
+                            >Services</NavLinks>
                         </NavItem>
                         <NavItem>
-                            <NavLinks to='signup'>Sign Up</NavLinks>
+                            <NavLinks to='signup'
+                                      smooth={true}
+                                      duration={500}
+                                      spy={true}
+                                      exact='true'
+                                      offet={-80}
+                            >Sign Up</NavLinks>
                         </NavItem>
                     </NavMenu>
                     <NavBtn>
@@ -40,6 +85,7 @@ const Navbar = ({ toggle }) => {
                     </NavBtn>
                 </NavbarContainer>
             </Nav>
+            </IconContext.Provider>
         </>
     );
 };
